fcb49750bfdfd481edf8a31521a7d3288628decd
Felipe Garavelli <felipegaravelli@gmail.com>
2023-07-12 14:29:18 -0700

    minor tweaks

--- lib/Constants/Constants.h
+++ lib/Constants/Constants.h
@@ -1,5 +1,27 @@
+#include <Arduino.h>
+
 #ifndef Constants_h
 #define Constants_h
-#endif
 
-#include <Arduino.h>
+//TODO: Make a namespace for each library and declare all constants for the library (stm32 pins)
+
+//Motors Name Space
+namespace MotorsNS {
+
+    //Example:
+    const int RIGHT_MOTOR = PA1;
+    const int LEFT_MOTOR = PA2;
+}
+
+
+//Reflectance Sensors Name Space
+namespace ReflectanceSensorsNS {
+
+}
+
+//Gyroscope Name Space
+namespace GyroscopeNS {
+
+}
+
+#endif
